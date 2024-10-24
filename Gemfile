5d11139becc7beb65afebf74767db8e75ea8cad8
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-24 16:06:25 +0000

    links created

--- Gemfile
+++ Gemfile
@@ -5,4 +5,10 @@ gem "puma"
 gem "rackup"
 gem "sinatra"
 gem "sinatra-contrib"
+gem "better_errors"
+gem "binding_of_caller"
+# Need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
--- dice.rb
+++ dice.rb
@@ -3,6 +3,18 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/") do
+  "
+  <h1>Dice Roll</h1>
+  <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+  </ul>
+  "
+end
+
 get ("/zebra") do
   "We must add a route for each path we want to support"
 end
@@ -21,3 +33,4 @@ get ("/dice/2/6") do
   "<h1>2d6</h1>
   <p>#{outcome}</p>"
 end 
+
