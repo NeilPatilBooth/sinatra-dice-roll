7e9083974af0d20b3eccfb35e0d15b8c9096f095
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-24 16:22:14 +0000

    2_d6 view

--- dice.rb
+++ dice.rb
@@ -4,15 +4,7 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+ erb(:elephant)
 end
 
 get ("/zebra") do
@@ -32,6 +24,7 @@ get ("/dice/2/6") do
 
   "<h1>2d6</h1>
   <p>#{outcome}</p>"
+  erb(:two_six)
 end 
 
 get("/dice/2/10") do
